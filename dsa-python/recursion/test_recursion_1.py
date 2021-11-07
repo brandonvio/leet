b0fced369d10a3eb2bc1f06e96ced08c4a26520f
Brandon Vicedomini <brandonvio@outlook.com>
2021-11-06 17:09:49 -0700

    Recursion. More.

--- dsa-python/recursion/test_recursion_1.py
+++ dsa-python/recursion/test_recursion_1.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 def test_recursion_fact():
     def fact(n):
         # base case
@@ -12,13 +15,9 @@ def test_recursion_fact():
             return result_fact
 
     result = fact(4)
-    print("")
-    print("fact(4):" + str(result))
     assert result == 24
 
     result = fact(5)
-    print("")
-    print("fact(5):" + str(result))
     assert result == 120
 
 
@@ -54,8 +53,6 @@ def test_recursion_sum2():
 
     result = rec_sum("4321")
     assert result == 10
-    print("")
-    print("rec_sum(4):" + str(result))
 
 
 def test_reverse_string():
@@ -74,7 +71,6 @@ def test_reverse_string():
 
     result = reverse("abcdef")
     assert result == "fedcba"
-    print('reverse("abcdef"):' + result)
 
 
 def test_permute_string():
@@ -93,28 +89,32 @@ def test_permute_string():
         return out
 
     result = permute("abc")
-    # assert result == "fedcba"
-    print('permute("abcd"):' + str(len(result)) + ", " + str(result))
+    assert result == ['abc', 'acb', 'bac', 'bca', 'cab', 'cba']
 
 
 def test_fibonacci_memo():
-    def fib(n: int, cache=None):
-        if cache is None:
-            cache = {}
+    def fib(n: int, memo=None):
+        if memo is None:
+            memo = {}
         # base case
         if n < 2:
             return n
+
+        # recursive case
         else:
-            if n in cache:
-                return cache[n]
+            if n in memo:
+                return memo[n]
             else:
-                val_left = fib(n - 1, cache)
-                val_right = fib(n - 2, cache)
-                cache[n] = val_left + val_right
-                return cache[n]
+                val_left = fib(n - 1, memo)
+                val_right = fib(n - 2, memo)
+                memo[n] = val_left + val_right
+                return memo[n]
 
     result = fib(10)
     assert result == 55
+    assert fib(1) == 1
+    assert fib(12) == 144
+    assert fib(23) == 28657
 
 
 def test_fibonacci():
@@ -122,6 +122,8 @@ def test_fibonacci():
         # base case
         if n < 2:
             return n
+
+        # recursive case
         else:
             val_left = fib(n - 1)
             val_right = fib(n - 2)
@@ -129,3 +131,58 @@ def test_fibonacci():
 
     result = fib(10)
     assert result == 55
+    assert fib(1) == 1
+    assert fib(23) == 28657
+
+
+def test_fibonacci_iter():
+    def fib(n: int):
+        a, b = 0, 1
+        for i in range(n):
+            a, b = b, a + b
+        return a
+
+    result = fib(10)
+    assert result == 55
+    assert fib(1) == 1
+    assert fib(23) == 28657
+
+
+def test_coin_change_recur():
+    def coin_change(target: int, coins: List[int]):
+        min_coins = target
+        if target in coins:
+            return 1
+        else:
+            for i in [c for c in coins if c <= target]:
+                num_coins = 1 + coin_change(target - i, coins)
+                if num_coins < min_coins:
+                    min_coins = num_coins
+        return min_coins
+
+    result = coin_change(12, [1, 5, 10])
+    assert result == 3
+
+
+def test_coin_change_memo():
+    def coin_change(target: int, coins: List[int], known_results: List[int]):
+        min_coins = target
+
+        if target in coins:
+            known_results[target] = 1
+            return 1
+
+        elif known_results[target] > 0:
+            return known_results[target]
+
+        else:
+            for i in [c for c in coins if c <= target]:
+                num_coins = 1 + coin_change(target - i, coins, known_results)
+                if num_coins < min_coins:
+                    min_coins = num_coins
+                    known_results[target] = min_coins
+
+        return min_coins
+
+    result = coin_change(74, [1, 5, 10, 25], [0] * 75)
+    assert result == 8
